210334463bfce5719c68bae6b726f40ea3c0f11f
Joy Gabriel <joysaragi@gmail.com>
2021-10-05 14:04:17 +0700

    Secure Java Spring API

---
+++ src/main/java/com/artajasa/javasecurecodingproject/JavaSecureCodingProjectApplication.java
@@ -0,0 +1,13 @@
+package com.artajasa.javasecurecodingproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaSecureCodingProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaSecureCodingProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/artajasa/javasecurecodingproject/JavaSecureCodingProjectApplicationTests.java
@@ -0,0 +1,13 @@
+//package com.artajasa.javasecurecodingproject;
+//
+////import org.junit.jupiter.api.Test;
+////import org.springframework.boot.test.context.SpringBootTest;
+////
+//@SpringBootTest
+//class JavaSecureCodingProjectApplicationTests {
+//
+//	@Test
+//	void contextLoads() {
+//	}
+//
+//}
